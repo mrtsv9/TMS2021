8d1c72eeef35c5d5ccebe372264e08aae5354b41
mrtsv <a.martsev85@gmail.com>
2021-04-01 03:06:37 +0300

    Uploaded

--- Lesson 2/src/Homework5.java
+++ Lesson 2/src/Homework5.java
@@ -3,9 +3,9 @@ public class Homework5 {
     public static void main(String[] args){
         double res = 0;
         Scanner input = new Scanner(System.in);
-        System.out.println("Enter th first cathet: ");
+        System.out.println("Enter the first cathet: ");
         double first = input.nextDouble();
-        System.out.println("Enter th second cathet: ");
+        System.out.println("Enter the second cathet: ");
         double second = input.nextDouble();
         res = Math.pow(Math.pow(first, 2) + Math.pow(second, 2), 0.5);
         System.out.println("And the hepotenuse is: " + res);
